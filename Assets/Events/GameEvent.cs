f1dc7c64e62428ec2b4f83864ad67af30d60d329
Kenneth <kennethlombardi@users.noreply.github.com>
2019-08-05 13:51:22 -0700

    Upgrade and create event system

---
+++ Assets/Events/GameEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events
+{
+    [CreateAssetMenu]
+    public class GameEvent : ScriptableObject
+    {
+        /// <summary>
+        /// The list of listeners that this event will notify if it is raised.
+        /// </summary>
+        private readonly List<GameEventListener> eventListeners =
+            new List<GameEventListener>();
+
+        public void Raise()
+        {
+            Debug.Log("yooo");
+            for (int i = eventListeners.Count - 1; i >= 0; i--)
+                eventListeners[i].OnEventRaised();
+        }
+
+        public void RegisterListener(GameEventListener listener)
+        {
+            if (!eventListeners.Contains(listener))
+                eventListeners.Add(listener);
+        }
+
+        public void UnregisterListener(GameEventListener listener)
+        {
+            if (eventListeners.Contains(listener))
+                eventListeners.Remove(listener);
+        }
+    }
+}
